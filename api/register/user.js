bd2d4bb89e25bb4f6ebe551b43a018266d208380
vivek <vivekbisht.111@gmail.com>
2021-03-11 22:43:26 +0530

    authentication added

--- api/register/user.js
+++ api/register/user.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const User = require('../../models/users');
 const {body,validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
+
+
+//app.use() won't work because the request is coming on the router's post function
+//ig
+router.use(cookieParser());
 
 router.post('/',body('username').isLength({min:3}),body('email').isEmail()
 ,body('password').isLength({min:3}),async (req,res)=>{
@@ -14,11 +21,6 @@ router.post('/',body('username').isLength({min:3}),body('email').isEmail()
         {
             return res.status(400).json({errors:errors.array()});
         }
-        // console.log(req.body.username);
-        // console.log(req.body.password);
-        // console.log(req.body.email);
-
-
 
         let u = new User({
             username : req.body.username,
@@ -26,13 +28,22 @@ router.post('/',body('username').isLength({min:3}),body('email').isEmail()
             email : req.body.email
         });
         
+        const token = await u.generateAuthToken();
+        console.log(token);
+        res.cookie("jwt",token,{
+            expires: new Date(Date.now()+60000),
+            httpOnly:true
+        });
+
+        //console.log(`Cookie Data.. ${req.cookies.jwt}`);
+    
+
         const salt = await bcrypt.genSalt(10);
         u.password = await bcrypt.hash(u.password,salt);
 
         await u.save();
         console.log(`user ${u.username} saved! `);
-        res.send(`user ${u.username} saved! `);
-
+        res.redirect('/');
     }
     catch(err){
         console.log(err);
@@ -42,7 +53,7 @@ router.post('/',body('username').isLength({min:3}),body('email').isEmail()
 router.put('/add/:pname',async(req,res)=>{
     try{
         //if not already present add the product with pname as name to cart of currently logged in user
-
+        
         
     }
     catch(err)
--- models/users.js
+++ models/users.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 const userSchema =  new mongoose.Schema({
     username : String,
     password: String,
     email : String,
     cart:[],
 });
+
+//methods keyword is used when the function needs to be called from
+//the instance of the particular schema
+userSchema.methods.generateAuthToken = async function(){
+    try{
+        const token = jwt.sign(
+            {_id:this._id.toString()},
+            "thisisasecretkeyneedstobehidden"
+        );
+        return token;
+
+    }
+    catch(err)
+    {
+        console.log("Error has occured:"+err);
+    }
+}
 
 const User = mongoose.model('User',userSchema);
 
